08af3be0369dac7466b223d11948dbeca20e6d85
Marcus Vinicius de Souza <marcus-vinicius1971@hotmail.com>
2020-04-02 11:55:30 -0300

    Alterações Básicas.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+   public todos: any[] = [];
+   public title: String= 'Minhas Tarefas';
 
+   constructor() {
+     this.todos.push('Estudar Angular');
+     this.todos.push('Iniciar front-end com angular');
+     this.todos.push('Estudar back-end com C#');
+     this.todos.push('Iniciar back-end com C#');
+   }
 }
+
