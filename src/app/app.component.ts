a2b090b2a0a23ed02b3f468656f1c9d0968672c5
Marcus Vinicius de Souza <marcus-vinicius1971@hotmail.com>
2020-04-06 12:18:56 -0300

    Inclusão das ações de Deletar, Concluido e Refazer.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,14 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   public todos: any[] = [];
+   public todos: Todo[] = [];
    public title: String= 'Minhas Tarefas';
 
    constructor() {
-     this.todos.push('Estudar Angular');
-     this.todos.push('Iniciar front-end com angular');
-     this.todos.push('Estudar back-end com C#');
-     this.todos.push('Iniciar back-end com C#');
+     this.todos.push(new Todo(1, 'Estudar Angular', true));
+     this.todos.push(new Todo(2, 'Iniciar front-end com angular', false));
+     this.todos.push(new Todo(3, 'Estudar back-end com C#', false));
+     this.todos.push(new Todo(4, 'Iniciar back-end com C#', false));
+   }
+
+   remove(todo: Todo) {
+     const index = this.todos.indexOf(todo);
+     if (index !== -1) {
+        this.todos.splice(index, 1);
+     }
+   }
+
+   markAsDone(todo: Todo) {
+     todo.done = true;
+   }
+
+   markAsUndone(todo: Todo) {
+     todo.done = false;
    }
 }
 
