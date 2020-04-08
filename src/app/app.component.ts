2c1c35047151e2b147531c0615ecb1d7be6be491
Marcus Vinicius de Souza <marcus-vinicius1971@hotmail.com>
2020-04-08 12:20:37 -0300

    Implementação do form e as ações de salvar e buscar dados usando o localStorage.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 import { Todo } from 'src/models/todo.model';
 
 @Component({
@@ -9,27 +11,59 @@ import { Todo } from 'src/models/todo.model';
 export class AppComponent {
    public todos: Todo[] = [];
    public title: String= 'Minhas Tarefas';
+   public form: FormGroup;
 
-   constructor() {
-     this.todos.push(new Todo(1, 'Estudar Angular', true));
-     this.todos.push(new Todo(2, 'Iniciar front-end com angular', false));
-     this.todos.push(new Todo(3, 'Estudar back-end com C#', false));
-     this.todos.push(new Todo(4, 'Iniciar back-end com C#', false));
+   constructor(private fb: FormBuilder) {
+     this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+     });
+
+     this.load();
+   }
+
+   add() {
+     const title = this.form.controls['title'].value;
+     const id = this.todos.length + 1;
+
+     this.todos.push(new Todo(id, title, false));
+
+     this.save();
+     this.form.reset();
    }
 
    remove(todo: Todo) {
      const index = this.todos.indexOf(todo);
      if (index !== -1) {
         this.todos.splice(index, 1);
-     }
+      }
+
+      this.save();
    }
 
    markAsDone(todo: Todo) {
      todo.done = true;
+
+     this.save();
    }
 
    markAsUndone(todo: Todo) {
      todo.done = false;
+
+     this.save();
+   }
+
+   save() {
+     const data = JSON.stringify(this.todos);
+     localStorage.setItem('todos', data);
+   }
+
+   load() {
+     const data = localStorage.getItem('todos');
+     this.todos = JSON.parse(data);
    }
 }
 
